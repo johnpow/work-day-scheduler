5d7e39cb8514a6783eb3313429e8ac8d6791c6a7
John Pow <johnowed@gmail.com>
2023-03-05 17:02:43 -0800

    created button listener to grab hour and text

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var saveBtnEl = $('.fa-save');
+
+
+
+saveBtnEl.on('click', function(event) {
+
+var btnClicked = $(event.target);
+
+let scheduleHour = btnClicked.parent().parent().attr('id');
+let scheduleInfo = btnClicked.parent().parent().children().eq(1).val()
+
+let scheduleEvents = {
+  student: scheduleHour.value,
+  grade: scheduleInfo.value,
+};
+
+});
+
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +30,12 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+
+
+
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
