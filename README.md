eb1fbca71f396d80eae44778ce6f7d0ea288c4c0
John Pow <johnowed@gmail.com>
2023-03-05 18:34:13 -0800

    added hour of day color coding, current day and readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Work Day Scheduler
+
+## Description
+This daily scheduler allows users to plan out there day by saving tasks each hour. You'll always know the current hour based on the colors which show current time (red), past time (grey) and future time (green). Even if the browser is closed/refreshed your tasks will stay put! 
+
+
+## Table of Contents
+- [Usage](#usage)
+- [Credits](#credits)
+- [Features](#features)
+- [Access](#access)
+
+## Usage
+- Populate the hour with desired text
+- Press 'Save' icon
+- Come back to the page at any time and your work will be saved
+
+![alt text](./cal.png)
+
+## Credits
+Pulled source code from [here](https://github.com/coding-boot-camp/crispy-octo-meme), as well as used examples from various Third-Party API trainings.
+
+## Features
+- The color coding makes it easy to know which hour you are in
+- Saving the tasks keep memory of what you've planned even if the browser closes
+
+## Access
+Click to view: [Work Day Scheduler](https://johnpow.github.io/work-day-scheduler/).
--- script.js
+++ script.js
@@ -2,60 +2,52 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var hourCal = [9,10,11]
-var saveBtnEl = $('.fa-save');
-
-saveBtnEl.on('click', function(event) {
-
-var btnClicked = $(event.target);
-
-let scheduleHour = btnClicked.parent().parent().attr('id');
-let scheduleInfo = btnClicked.parent().parent().children().eq(1).val()
-
-console.log(scheduleHour + ' ' + scheduleInfo)
-
-localStorage.setItem(scheduleHour, JSON.stringify(scheduleInfo));
-
-});
-
-
-hourCal.forEach(function(item) {
-
-let hourSaved = $(`#hour-${item}`);
-let hourLogged = JSON.parse(localStorage.getItem(`hour-${item}`));
-
-if (!hourLogged) {
-  hourSaved.children().eq(1).val('')
-} else {
-  hourSaved.children().eq(1).val(`${hourLogged}`)
-}
-
-});
-
+var hourCal = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var saveBtnEl = $(".fa-save");
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // listener for click events on the save button
 
+  saveBtnEl.on("click", function (event) {
+    var btnClicked = $(event.target);
 
+    let scheduleHour = btnClicked.parent().parent().attr("id");
+    let scheduleInfo = btnClicked.parent().parent().children().eq(1).val();
 
+    // console.log(scheduleHour + ' ' + scheduleInfo)
 
-
+    localStorage.setItem(scheduleHour, JSON.stringify(scheduleInfo));
+  });
 
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // apply the past, present, or future class to each time
+  // block by comparing the id to the current hour
+
+  hourCal.forEach(function (item) {
+    let currentHour = dayjs().hour();
+    if (item === currentHour) {
+      $(`#hour-${item}`).attr("class", "row time-block present");
+    } else if (item > currentHour) {
+      $(`#hour-${item}`).attr("class", "row time-block future");
+    } else {
+      $(`#hour-${item}`).attr("class", "row time-block past");
+    }
+  });
+
+  // Get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements.
+
+  hourCal.forEach(function (item) {
+    let hourSaved = $(`#hour-${item}`);
+    let hourLogged = JSON.parse(localStorage.getItem(`hour-${item}`));
+
+    if (!hourLogged) {
+      hourSaved.children().eq(1).val("");
+    } else {
+      hourSaved.children().eq(1).val(`${hourLogged}`);
+    }
+  });
+
+  // Display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 });
